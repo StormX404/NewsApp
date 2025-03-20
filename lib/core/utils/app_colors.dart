0a58ec996ebeeef10e29728ad2ca374948805420
AbdoYasser74 <130872766+AbdoYasser74@users.noreply.github.com>
2025-03-20 16:16:30 +0200

    add  text styles and colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+class AppColors{
+
+// App theme colors
+  static const Color primary = Color(0xFFEE5253);
+
+  // Background Colors
+  static const Color light = Color(0xFFFFFFFF);
+  static const Color dark = Color(0xFF18182F);
+
+  // Text Colors
+  static const Color lightTextPrimary = Color(0xFF18182F);
+  static const Color lightTextSecondary = Color(0xFF90909b);
+  static const Color darkTextPrimary = Color(0xFFFFFFFF);
+  static const Color darkTextSecondary = Color(0xFF878793);
+
+  // Button colors
+  static const Color buttonPrimary = Color(0xFFEE5253);
+
+  // Border colors
+  static const Color borderPrimary = Color(0xFFD9D9D9);
+
+  // Error and validation colors
+  static const Color error = Color(0xFFD32F2F);
+  static const Color success = Color(0xFF388E3C);
+  static const Color warning = Color(0xFFF57C00);
+  static const Color info = Color(0xFF1976D2);
+
+  // Neutral Shades
+  static const Color black = Color(0xFF232323);
+  static const Color darkerGrey = Color(0xFF4F4F4F);
+  static const Color darkGrey = Color(0xFF939393);
+  static const Color grey = Color(0xFFE0E0E0);
+  static const Color softGrey = Color(0xFFF4F4F4);
+  static const Color lightGrey = Color(0xFFF9F9F9);
+  static const Color white = Color(0xFFFFFFFF);
+
+
+
+}
---
+++ lib/core/utils/app_text_styles.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+
+  // Regular Text Styles
+  static const TextStyle regular12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+  );
+
+
+  static const TextStyle regular14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle regular18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle regular20 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle regular24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.normal,
+  );
+
+  // Medium Text Styles
+  static const TextStyle medium12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle medium14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle medium18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle medium20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle medium24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+  );
+
+
+  // Bold Text Styles
+  static const TextStyle bold12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle bold14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle bold18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle bold20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle bold24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+
+}
