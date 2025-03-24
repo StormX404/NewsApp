bb54be53cef18adf5aee483ecd7971ac7341ec46
AbdoYasser74 <130872766+AbdoYasser74@users.noreply.github.com>
2025-03-25 00:23:28 +0200

    create news repo

---
+++ lib/features/news/domain/repos/news_repo.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:news_app/core/errors/failure.dart';
+import 'package:news_app/features/news/domain/entities/news_entity.dart';
+
+abstract class NewsRepo {
+  Future<Either<Failure, List<NewsEntity>>> fetchAllNews({int pageNumber = 0});
+
+  Future<Either<Failure, List<NewsEntity>>> fetchNewsWithCategory(
+      {String category = 'general' , int pageNumber = 0});
+}
