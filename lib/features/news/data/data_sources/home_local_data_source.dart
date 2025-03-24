d06d91d4931e074e41ff2ab6480e2e65d8b476a7
AbdoYasser74 <130872766+AbdoYasser74@users.noreply.github.com>
2025-03-25 00:25:31 +0200

    create home local data source

---
+++ lib/features/news/data/data_sources/home_local_data_source.dart
@@ -0,0 +1,29 @@
+import 'package:hive/hive.dart';
+import 'package:news_app/core/utils/constants/box_names.dart';
+import 'package:news_app/features/news/domain/entities/news_entity.dart';
+
+abstract class HomeLocalDataSource {
+  List<NewsEntity> fetchAllNews({int pageNumber = 0});
+  List<NewsEntity> fetchNewsWithCategory({required String category});
+}
+
+class HomeLocalDataSourceImpl extends HomeLocalDataSource {
+  @override
+  List<NewsEntity> fetchAllNews({int pageNumber = 0}) {
+    int startIndex = pageNumber * 10;
+    int endIndex = (pageNumber + 1) * 10;
+
+    var box = Hive.box<NewsEntity>(kAllNewsBox);
+    int length = box.values.length;
+    if (startIndex >= length || endIndex > length) {
+      return [];
+    }
+    return box.values.toList().sublist(startIndex, endIndex);
+  }
+
+  @override
+  List<NewsEntity> fetchNewsWithCategory({required String category , int pageNumber = 0}) {
+    var box = Hive.box<NewsEntity>(kCategoryNewsBox);
+    return box.values.toList();
+  }
+}
